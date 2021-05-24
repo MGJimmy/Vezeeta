9e7a443df7a7444cdecb0f9e4fe0996d2c751bbb
mgamal_jimmy <mohamedgamal5907@gmail.com>
2021-05-24 20:34:08 +0200

    angular structure v1.0

---
+++ Consumer/ClientApp/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }

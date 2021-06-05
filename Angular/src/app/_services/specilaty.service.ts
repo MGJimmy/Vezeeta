40df7b37d45b13b58c6fc5260324ea80fc9b645a
mahmoudfarrag <mahmoud.farrag19941994@gmail.com>
2021-06-05 18:41:21 +0200

    update speclity

--- Angular/src/app/_services/specilaty.service.ts
+++ Angular/src/app/_services/specilaty.service.ts
@@ -11,7 +11,7 @@ import { ISpecialty } from '../_models/_interfaces/ISpecilaty';
 export class SpecilatyService {
 
   constructor(private _http:HttpClient) { }
-  getAllCategories():Observable<ISpecialty[]> {
+  getAllSpecialities():Observable<ISpecialty[]> {
     let url = `${environment.apiUrl}/api/Specialty`;
     return this._http.get<ISpecialty[]>(url).pipe(catchError((err)=>
     {
@@ -32,7 +32,7 @@ export class SpecilatyService {
       return throwError(err.message ||"Internal Server error contact site adminstarator");
     }));
   }
-  getCategoryById(id:number):Observable<ISpecialty>{
+  getSpecialityById(id:number):Observable<ISpecialty>{
     let url = `${environment.apiUrl}/api/Specialty/${id}`;
     return this._http.get<ISpecialty>(url).pipe(catchError((err)=>
     {
